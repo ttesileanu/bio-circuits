12b2a59a2cf0f374cb9710768f6ce4e8aca740ec
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2022-07-02 18:10:26 -0400

    Fix package name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name="bio-circuits",
+    name="biocircuits",
     version="0.0.1",
     author="Tiberiu Tesileanu",
     author_email="ttesileanu@flatironinstitute.org",
     url="https://github.com/ttesileanu/bio-circuits",
-    packages=["bio-circuits"],
+    packages=["biocircuits"],
     package_dir={"": "src"},
     install_requires=[
         "numpy",
